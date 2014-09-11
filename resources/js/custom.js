119c43106678fccbc10014a7c944db74952cf8a3
unknown <edmund.quintero@cpcc.edu>
2014-09-11 13:43:02 -0400

    Added profile page and Js controls. updated CSS

---
+++ resources/js/custom.js
@@ -0,0 +1,17 @@
+$(function(){
+  'use strict';
+
+  $('.datepicker').datepicker();
+
+
+  $('.collapsable-header').on('click', function(){
+    var $target = $($(this).attr('data-target'));
+    if($target.is(':visible')){
+      $target.slideUp();
+      $(this).removeClass('open');
+    } else {
+      $target.slideDown();
+      $(this).addClass('open');
+    }
+  });
+});
