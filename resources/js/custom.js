848738fc6a1ecfd7d4fec6e1e8a51a1181b2463e
Quintero, Edmund <Edmund.Quintero@sungard.com>
2014-11-06 10:30:08 -0500

    Navigation though

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -15,4 +15,35 @@ $(function(){
     }
   });
 
+
+  $('nav.top-top > ul').on('mouseenter', 'li', function(){
+    $($(this).children('ul')[0]).addClass('submenuShow');
+  });
+  $('nav.top-top > ul').on('mouseleave', 'li', function(){
+    $($(this).children('ul')[0]).removeClass('submenuShow');
+  });
+
+  $('.profile-menu').on('click', function(){
+    var overlay = $(document.createElement('div'));
+    overlay.addClass('overlay profile-overlay');
+    overlay.on('click', function(){
+      $('nav.profile').removeClass('active');
+      overlay.remove();
+    })
+    $('nav.profile').on('click', '.closeProfile', function(){
+      $('nav.profile').removeClass('active');
+      overlay.remove();
+    });
+
+    $('nav.profile').addClass('active');
+    $('body').append(overlay);
+  });
+
+
+  $('.dropup-menu').on('click', '.dropup-toggle', function(){
+    console.log('dropup clicked', $(this).next('ul'));
+    $(this).next('ul').toggleClass('active');
+  });
+
+
 });
